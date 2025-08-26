b1c4dc4d4c2673217b13fa5f2048b841de8d4ce7
taimats <beo8939@gmail.com>
2025-08-26 21:44:40 +0900

    add: loginHandler test

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -3,12 +3,15 @@ package handler_test
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/taimats/ninka/handler"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type testRequest struct {
@@ -20,6 +23,47 @@ type testRequest struct {
 type wantResponse struct {
 	body       string
 	statusCode int
+}
+
+func testHandler(t *testing.T, h http.HandlerFunc, r *http.Request) (res *http.Response, body string) {
+	t.Helper()
+
+	w := httptest.NewRecorder()
+	h(w, r)
+
+	res = w.Result()
+	var buf bytes.Buffer
+	io.Copy(&buf, res.Body)
+
+	return res, buf.String()
+}
+
+func newJSONBody[T any](t *testing.T, v T) io.Reader {
+	var r bytes.Buffer
+	err := json.NewEncoder(&r).Encode(v)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return &r
+}
+
+func newJSONString[T any](t *testing.T, v T) string {
+	var r bytes.Buffer
+	err := json.NewEncoder(&r).Encode(v)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return r.String()
+}
+
+func cleanupStore[K comparable, V any](t *testing.T, s *handler.Store[K, V]) {
+	t.Helper()
+	for k := range s.Data {
+		delete(s.Data, k)
+	}
+	if len(s.Data) != 0 {
+		t.Fatalf("failed to clean up %sStore\n", s.Name)
+	}
 }
 
 func TestRegisterHandler(t *testing.T) {
@@ -67,6 +111,9 @@ func TestRegisterHandler(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.desc, func(t *testing.T) {
+			t.Cleanup(func() {
+				cleanupStore(t, handler.ClientStore)
+			})
 			req, err := http.NewRequest(tt.req.method, tt.req.path, newJSONBody(t, tt.req.body))
 			if err != nil {
 				t.Fatal(err)
@@ -84,33 +131,69 @@ func TestRegisterHandler(t *testing.T) {
 	}
 }
 
-func testHandler(t *testing.T, h http.HandlerFunc, r *http.Request) (res *http.Response, body string) {
-	t.Helper()
-
-	w := httptest.NewRecorder()
-	h(w, r)
-
-	res = w.Result()
-	var buf bytes.Buffer
-	io.Copy(&buf, res.Body)
-
-	return res, buf.String()
-}
-
-func newJSONBody[T any](t *testing.T, v T) io.Reader {
-	var r bytes.Buffer
-	err := json.NewEncoder(&r).Encode(v)
+func TestLoginHandler(t *testing.T) {
+	cl := handler.NewClient("test", "test_secret", "http://localhost:8080/test")
+	hashed, err := bcrypt.GenerateFromPassword([]byte("test_secret"), bcrypt.DefaultCost)
 	if err != nil {
 		t.Fatal(err)
 	}
-	return &r
-}
-
-func newJSONString[T any](t *testing.T, v T) string {
-	var r bytes.Buffer
-	err := json.NewEncoder(&r).Encode(v)
+	cl.Secret = handler.Password(hashed)
+	err = handler.ClientStore.Add(cl.ID, cl)
 	if err != nil {
 		t.Fatal(err)
 	}
-	return r.String()
+	q := &url.Values{}
+	q.Add("id", "test")
+	q.Add("secret", "test_secret")
+	q.Add("redirect", "http://localhost:8080/test")
+
+	tests := []struct {
+		desc string
+		req  testRequest
+		want wantResponse
+	}{
+		{
+			desc: "pass_01",
+			req: testRequest{
+				method: "POST",
+				path:   fmt.Sprintf("/login?%s", q.Encode()),
+				body:   "",
+			},
+			want: wantResponse{
+				body:       "",
+				statusCode: http.StatusFound,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			t.Cleanup(func() {
+				cleanupStore(t, handler.ClientStore)
+			})
+
+			req, err := http.NewRequest(tt.req.method, tt.req.path, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			res, body := testHandler(t, handler.LoginHandler, req)
+
+			if body != tt.want.body {
+				t.Errorf("Body is not equal: (got=%s, want=%s)\n", body, tt.want.body)
+			}
+			if res.StatusCode != tt.want.statusCode {
+				t.Errorf("StatusCode is not equal: (got=%d, want=%d)\n", res.StatusCode, tt.want.statusCode)
+			}
+			if len(res.Cookies()) == 0 {
+				t.Errorf("Cookie is empty: (length=%d)", len(res.Cookies()))
+			}
+			url, err := res.Location()
+			if err != nil {
+				t.Errorf("redirect location is not set")
+			}
+			if url.String() != "http://localhost:8080/test" {
+				t.Errorf("redirect location is not equal: (got=%s, want=%s)", url.String(), "http://localhost:8080/test")
+			}
+		})
+	}
 }
